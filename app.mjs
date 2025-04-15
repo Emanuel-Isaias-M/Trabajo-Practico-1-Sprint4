8e73fce38df11298631bafe44a66d72544c87437
Emanuel-Isaias-M <moralezisaias.1@gmail.com>
2025-04-14 23:05:22 -0300

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -51,6 +51,6 @@ app.use((req, res) => {
 });
 
 // Iniciar servidor
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
